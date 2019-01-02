579302e42d777a4628e00d264ecde393d3caa37e
Manish <manishcspatel@gmail.com>
2019-01-02 22:29:49 +0530

    chnge in b_url with http

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'sjesgzp.com';
+$config['base_url']	= 'http://sjesgzp.com';
 
 /*
 |--------------------------------------------------------------------------
